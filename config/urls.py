09d91456773f65627fbd897dafc62baab17ce212
gladtycoon <gladtycoon@gmail.com>
2026-04-06 04:40:32 +0200

    fixed config/urls.py and add readme.md  and requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from catalog import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    re_path(r'^contacts/$', views.contacts, name='contacts'),
+    path('', include('catalog.urls')),
 ]
