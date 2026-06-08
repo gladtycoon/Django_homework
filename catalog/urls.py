02afcadc898fbb7977e17f7031af3fcac49cccac
gladtycoon <gladtycoon@gmail.com>
2026-06-08 22:43:21 +0200

    add products  pages, custom tags, templates and static

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,13 @@
 # from django.contrib import admin
-# from django.urls import include, path
+from django.urls import include, path
+
+from catalog.views import product_detail, products_list
+
+from . import views
 
 app_name = "catalog"
 
-urlpatterns = []
+urlpatterns = [
+    path("", products_list, name="products_list"),
+    path("products/<int:pk>", product_detail, name="product_detail"),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Product
 
 
 def home(request):
@@ -17,3 +19,15 @@ def contacts(request):
         message = request.POST.get("message")
         return HttpResponse(f"Спасибо, {name}! Мы свяжемся с вами.")
     return render(request, "contacts.html")
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "catalog/products_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
