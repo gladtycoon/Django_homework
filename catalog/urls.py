29b0dbd197a77588b69ca2c0a824b6c59784ecad
Ivan Sokolskiy <gladtycoon@gmail.com>
2026-06-18 19:14:05 +0200

    used CBV and add newapp blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,10 @@
-# from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-from catalog.views import product_detail, products_list
-
-from . import views
+from catalog.views import ProductDetailView, ProductListView
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("", products_list, name="products_list"),
-    path("products/<int:pk>", product_detail, name="product_detail"),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
